4d49ebf80694fc8a94b978ba7ae390abcc395890
Ryo <ryosugarmed@gmail.com>
2024-06-20 18:56:37 +0900

    デバイスカスタマイズ　メール機能実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -73,5 +74,17 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.action_mailer.default_url_options = { protocol: 'https', host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { protocol: 'http', host: 'localhost', port: 3000 }
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'smtp.gmail.com',
+    user_name:            ENV['GMAIL_ADDRESS'],
+    password:             ENV['GMAIL_PASS'],
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  resources :admins
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+    root :to => "devise/sessions#new"
+    end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 end
