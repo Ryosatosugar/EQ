7449f14fef13d56d81e92242e2cb7cc4a7737205
Ryo <ryosugarmed@gmail.com>
2024-07-11 15:10:39 +0900

    FormQuestionモデル作成

---
+++ db/migrate/20240711060817_create_form_questions.rb
@@ -0,0 +1,12 @@
+class CreateFormQuestions < ActiveRecord::Migration[6.1]
+  def change
+    create_table :form_questions do |t|
+      t.string :name_form_question
+      t.string :subject_form_question
+      t.text :content_form_question
+      t.string :image_form_question
+
+      t.timestamps
+    end
+  end
+end
