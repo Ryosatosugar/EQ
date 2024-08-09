16346ca2b5f622b7040c2070b6a3af6e79655cac
Ryo <ryosugarmed@gmail.com>
2024-08-09 18:17:02 +0900

    adminsコントローラーを使用して、マイページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -7,21 +7,20 @@ Rails.application.routes.draw do
   get "admin_users" => "admins#admin_users"
   get "admin_questions" =>"admins#admin_questions"
 
-
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
-
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
     #root :to => "devise/sessions#new"
-    end
+  end
+ get "users/:id" => "admins#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   #namespace :administrator do
     #resources :users, only: [:index]
-  end
+  # end
 
 
-#end
+end
