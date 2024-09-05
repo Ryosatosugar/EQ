8ec601627eb6c36eb04fde630256075a0a5c1f59
Ryo <ryosugarmed@gmail.com>
2024-09-05 16:30:47 +0900

    トップページに検索フォームを実装

--- app/controllers/core_pages_controller.rb
+++ app/controllers/core_pages_controller.rb
@@ -1,5 +1,7 @@
 class CorePagesController < ApplicationController
   def top
     @form_questions = FormQuestion.all
+    @q = FormQuestion.ransack(params[:q])
+    @questions = @q.result(distinct: true)
   end
 end
