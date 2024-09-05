6cc373c77a9f29b828486642d1f338567bf56ede
Ryo <ryosugarmed@gmail.com>
2024-09-05 17:03:36 +0900

    検索機能をheaderに移動

--- app/controllers/core_pages_controller.rb
+++ app/controllers/core_pages_controller.rb
@@ -1,7 +1,5 @@
 class CorePagesController < ApplicationController
   def top
     @form_questions = FormQuestion.all
-    @q = FormQuestion.ransack(params[:q])
-    @questions = @q.result(distinct: true)
   end
 end
