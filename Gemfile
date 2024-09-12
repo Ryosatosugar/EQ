760b7acccb9455051d2872a39773c089124f7c30
Ryo <ryosugarmed@gmail.com>
2024-09-12 18:12:12 +0900

    質問、回答フォーム中の必要ない部分を非表示化

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
+
+
 gem 'ransack'
 
 group :development, :test do
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Eq
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     config.i18n.default_locale = :ja
 
    # I18nライブラリに訳文の探索場所を指示する
