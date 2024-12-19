d9e1a167ea61f4b13b44a48034eac8ebdb5eecbd
Ryo <ryosugarmed@gmail.com>
2024-12-19 15:26:04 +0900

    printimage機能の大枠完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get "print_images/user_index" => "print_images#user_index"
-  get "print_images/user_show" => "print_images#user_show"
+  get "print_images/user_show/:id", to: "print_images#user_show", as: "print_images_user_show"
   get "single_posts/user_index" => "single_posts#user_index"
   get "single_posts/user_show" => "single_posts#user_show"
 
