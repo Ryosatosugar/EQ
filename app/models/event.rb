0de13654d9a4c01c17460adccf9b09faf9341cca
Ryo <ryosugarmed@gmail.com>
2024-12-26 18:39:24 +0900

    print_imageの検索機能実装

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
     has_many :print_images
+
+    def self.ransackable_attributes(auth_object = nil)
+        ["created_at", "event_name", "id", "updated_at"]
+    end
 end
