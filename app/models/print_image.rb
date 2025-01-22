3f116d26249fa93507c58945f38ae93a7e288eff
Ryo <ryosugarmed@gmail.com>
2025-01-22 18:07:49 +0900

    エラー修正

--- app/models/print_image.rb
+++ app/models/print_image.rb
@@ -4,4 +4,7 @@ class PrintImage < ApplicationRecord
   belongs_to :event
   has_one_attached :thumbnail_image
   has_many_attached :image_for_download
+
+  validates :category_id, presence: true
+  validates :event_id, presence: true
 end
