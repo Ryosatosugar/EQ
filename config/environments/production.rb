7b280903a9095a4673490547c4f7b8d26ae9ff40
Ryo <ryosugarmed@gmail.com>
2025-02-07 11:16:02 +0900

    config enviroments production.rbにホスト認証について記述を追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # ホスト認証: Heroku のホスト名を許可する
+  config.hosts << "eq-app-b952302c7a99.herokuapp.com"
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
