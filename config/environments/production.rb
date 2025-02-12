ce64a2999a911748aad3f5e195ca4279dd36bd73
Kemmochi <t.kenmochi.1985@gmail.com>
2025-02-12 13:03:59 +0900

    config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,23 +3,13 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-
   config.force_ssl = true
-
-
 
   # ホスト認証: Heroku のホスト名を許可する
   config.hosts << "eq-app-b952302c7a99.herokuapp.com"
 
-
-
-
-
-
-  config.action_controller.asset_host = 'https://eq-app.herokuapp.com'
+  config.action_controller.asset_host = 'https://eq-app-b952302c7a99.herokuapp.com/'
   config.action_mailer.asset_host = config.action_controller.asset_host
-
-
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -47,6 +37,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.digest = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
@@ -54,6 +45,21 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+
+
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'eq-app-b952302c7a99.herokuapp.com'}
+
+  config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'smtp.gmail.com',
+    user_name:            ENV['GMAIL_ADDRESS'],
+    password:             ENV['GMAIL_PASS'],
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
@@ -64,7 +70,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
