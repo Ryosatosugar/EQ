8103c2b3b986ae3f199bdffe0525b3c698003aa3
Ryo <ryosugarmed@gmail.com>
2025-04-02 14:18:51 +0900

    修正

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,5 +2,6 @@
 
 pin "application", preload: true
 pin "popper", to: 'popper.js', preload: true
-pin "bootstrap", to: 'bootstrap.min.js', preload: true
+pin "bootstrap" # @5.3.3
 
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
