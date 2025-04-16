81ceb04bc40755a9cbc410afcb79e66a8c963096
Ryo <ryosugarmed@gmail.com>
2025-04-16 17:47:56 +0900

    stimulusで実装

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,5 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import { application } from "./application";
+import { registerControllers } from "@hotwired/stimulus-loading";
+
+const controllers = import.meta.glob("./**/*_controller.js");
+registerControllers(application, controllers);
