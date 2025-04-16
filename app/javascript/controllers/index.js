be096da48dbf1be2de6921809e75d51efa9b13de
Ryo <ryosugarmed@gmail.com>
2025-04-16 16:25:50 +0900

    本番環境のコンパイル実行可能に

---
+++ app/javascript/controllers/index.js
@@ -0,0 +1,5 @@
+// app/javascript/controllers/index.js
+// 空でもOK、もしくは Stimulus を使う場合は↓のように記述
+// import { application } from "controllers/application"
+// import HelloController from "./hello_controller"
+// application.register("hello", HelloController)
