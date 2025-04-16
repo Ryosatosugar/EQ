ffdd729c2b5ac44a2a02cf6760f8d7d2053e1897
Ryo <ryosugarmed@gmail.com>
2025-04-16 16:25:50 +0900

    本番環境のコンパイル実行可能に

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
+pin "controllers", to: "controllers/index.js"
 pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
 pin "bootstrap" # @5.3.5
 pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @2.1.0
