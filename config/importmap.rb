8b1ede0111ce8d2c245fb1f5b6a3c4234f731e26
Ryo <ryosugarmed@gmail.com>
2025-04-16 17:47:56 +0900

    stimulusで実装

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,7 @@ pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @2.1.0
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
